8cf7ccb2f547e1d874be521561cb3b0bcf316c41
RainBowT0506 <rainbowt0506@gmail.com>
2024-07-18 11:15:11 +0800

    Initial Commit

---
+++ SwiftfulThinking-FirebaseBootcamp/SwiftfulThinking_FirebaseBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinking_FirebaseBootcampApp.swift
+//  SwiftfulThinking-FirebaseBootcamp
+//
+//  Created by RainBowT on 2024/7/18.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinking_FirebaseBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
